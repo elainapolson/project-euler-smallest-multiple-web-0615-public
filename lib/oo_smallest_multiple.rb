8f2e00683128b2bb958872b2fbaeadb3c3063836
elainapolson <elainapolson@gmail.com>
2015-08-31 12:14:52 -0400

    works but so slow

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,35 @@
 # Implement your object-oriented solution here!
+
+class SmallestMultiple
+  
+  def initialize(divisor)
+    @divisor = divisor
+    @answer = 1
+  end
+
+  def find_smallest_multiple
+    until is_divisible(@answer, @divisor)
+      @answer += 1
+    end
+  end
+
+  def is_divisible(answer, divisor)
+    collect_values = []
+    divisor.downto(1) do |x|
+      if answer % x != 0
+        collect_values << false
+      end
+    end
+    if collect_values.include?(false)
+      false
+    else
+      true
+    end
+  end
+
+  def lcm
+    find_smallest_multiple
+    return @answer
+  end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+
+def smallest_multiple(divisor)
+  answer = 1
+
+  until is_divisible(answer, divisor)
+    answer += 1
+  end
+
+  return answer
+    
+end
+
+def is_divisible(answer, divisor)
+  collect_values = []
+  divisor.downto(1) do |x|
+    if answer % x != 0
+      collect_values << false
+    end
+  end
+  if collect_values.include?(false)
+    false
+  else
+    true
+  end
+end
