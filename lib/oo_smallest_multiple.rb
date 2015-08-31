79ce8b35e7e475b0df439d0748459e1f899a5dbe
elainapolson <elainapolson@gmail.com>
2015-08-31 12:16:49 -0400

    refactor a few lines, still slow

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -14,17 +14,12 @@ class SmallestMultiple
   end
 
   def is_divisible(answer, divisor)
-    collect_values = []
     divisor.downto(1) do |x|
       if answer % x != 0
-        collect_values << false
+        return false
       end
     end
-    if collect_values.include?(false)
-      false
-    else
-      true
-    end
+    return true
   end
 
   def lcm
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -12,15 +12,10 @@ def smallest_multiple(divisor)
 end
 
 def is_divisible(answer, divisor)
-  collect_values = []
   divisor.downto(1) do |x|
     if answer % x != 0
-      collect_values << false
+      return false
     end
   end
-  if collect_values.include?(false)
-    false
-  else
-    true
-  end
+  return true
 end
